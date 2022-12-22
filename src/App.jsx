0192eaf896551065f64539cbd6502d6baaab06fa
norly96 <norly021196@gmail.com>
2022-12-22 16:48:49 -0500

    principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Header from "./components/Header"
+import FormCard from "./components/FormCard"
+import { Container } from "react-bootstrap"
+import CardsProvider from "./context/CardContext"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <CardsProvider>
+      <Header/>
+      <Container>
+        <FormCard/>
+      </Container>  
+    </CardsProvider>
   )
 }
 
-export default App
+export default App;
