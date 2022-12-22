da337e6a17007bc56135694dfe7505bfca333f7e
norly96 <norly021196@gmail.com>
2022-12-22 18:16:45 -0500

    fix marks

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,88 +1,88 @@
 const Icons = (icon) => {
     switch (icon) {
-        case '1':
-            icon='icons/wi-day-cloudy.svg'
+        case "1":
+            icon="icons/wi-day-cloudy.svg"
             break; 
-        case '2':
-            icon='icons/wi-day-cloudy.svg'
+        case "2":
+            icon="icons/wi-day-cloudy.svg"
             break;
         case '3':
-            icon='icons/wi-day-cloudy.svg'
+            icon="icons/wi-day-cloudy.svg"
             break;               
-        case '45':
-            icon='icons/wi-day-fog.svg'
+        case "45":
+            icon="icons/wi-day-fog.svg"
             break;
-        case '48':
-            icon='icons/wi-day-fog.svg'
+        case "48":
+            icon="icons/wi-day-fog.svg"
             break;    
-        case '51':
-            icon='icons/wi-day-hail.svg'
+        case "51":
+            icon="icons/wi-day-hail.svg"
             break;  
-        case '53':
-            icon='icons/wi-day-hail.svg'
+        case "53":
+            icon="icons/wi-day-hail.svg"
             break; 
-        case '55':
-            icon='icons/wi-day-hail.svg'
+        case "55":
+            icon="icons/wi-day-hail.svg"
             break;                               
-        case '56':
-            icon='icons/wi-night-alt-sleet.svg'
+        case "56":
+            icon="icons/wi-night-alt-sleet.svg"
             break;
-        case '57':
-            icon='icons/wi-night-alt-sleet.svg'
+        case "57":
+            icon="icons/wi-night-alt-sleet.svg"
             break;     
-        case '61':
-            icon='icons/wi-rain-mix.svg'
+        case "61":
+            icon="icons/wi-rain-mix.svg"
             break;  
-        case '63':
-            icon='icons/wi-rain-mix.svg'
+        case "63":
+            icon="icons/wi-rain-mix.svg"
             break;
-        case '65':
-            icon='icons/wi-rain-mix.svg'
+        case "65":
+            icon="icons/wi-rain-mix.svg"
             break;        
-        case '66':
-            icon='icons/wi-rain.svg'
+        case "66":
+            icon="icons/wi-rain.svg"
             break; 
-        case '67':
-            icon='icons/wi-rain.svg'
+        case "67":
+            icon="icons/wi-rain.svg"
             break;     
-        case '71':
-            icon='icons/wi-night-snow.svg'
+        case "71":
+            icon="icons/wi-night-snow.svg"
             break;    
-        case '73':
-            icon='icons/wi-night-snow.svg'
+        case "73":
+            icon="icons/wi-night-snow.svg"
             break; 
-        case '75':
-            icon='icons/wi-night-snow.svg'
+        case "75":
+            icon="icons/wi-night-snow.svg"
             break;        
-        case '77':
-            icon='icons/wi-snow.svg'
+        case "77":
+            icon="icons/wi-snow.svg"
             break;   
-        case '80':
-            icon='icons/wi-day-showers.svg'
+        case "80":
+            icon="icons/wi-day-showers.svg"
             break; 
-        case '81':
-            icon='icons/wi-day-showers.svg'
+        case "81":
+            icon="icons/wi-day-showers.svg"
             break;  
-        case '82':
-            icon='icons/wi-day-showers.svg'
+        case "82":
+            icon="icons/wi-day-showers.svg"
             break;           
-        case '85':
-            icon='icons/wi-snow-wind.svg'
+        case "85":
+            icon="icons/wi-snow-wind.svg"
             break;
-        case '86':
-            icon='icons/wi-snow-wind.svg'
+        case "86":
+            icon="icons/wi-snow-wind.svg"
             break;    
-        case '95':
-            icon='icons/wi-thunderstorm.svg'
+        case "95":
+            icon="icons/wi-thunderstorm.svg"
             break;
-        case '96':
-            icon='icons/wi-night-thunderstorm.svg'
+        case "96":
+            icon="icons/wi-night-thunderstorm.svg"
             break;  
-        case '99':
-            icon='icons/wi-night-thunderstorm.svg'
+        case "99":
+            icon="icons/wi-night-thunderstorm.svg"
             break;            
-        default:
-            icon='icons/wi-day-sunny.svg'   
+        default:  
+            icon="icons/wi-day-sunny.svg"   
     }
 return icon
 }
